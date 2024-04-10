053488237e8dc78c464fa65ee80ea665e5ec3d77
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-10 15:32:15 +0200

    Updated StateMachine.cs with minor comments tweeks

--- Scripts/Characters/StateMachine.cs
+++ Scripts/Characters/StateMachine.cs
@@ -15,13 +15,13 @@ public partial class StateMachine : Node
 
     public override void _Ready()
     {
-        // sent notification of the current state 
+        // send notification of the current state 
         _currentState.Notification(NotificationConsts.ENTER_STATE);
     }
 
 
     /// <summary>
-    /// Loop through the states array and chane the current state from the previouse to the new state<br/>
+    /// Loop through the states array and change the current state from the previouse to the new state<br/>
     /// Then fire a notification for the previouse state to disable and another notification for the current state to play
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -39,7 +39,7 @@ public partial class StateMachine : Node
 
         if (newState != null)
         {
-            // Befor change to the new state disable the previouse one
+            // Before changing to the new state disable the previouse one
             _currentState.Notification(NotificationConsts.EXIT_STATE);
             _currentState = newState;
             _currentState.Notification(NotificationConsts.ENTER_STATE);
