4760f8368632210c3cb0b2f0286d0554c50c003d
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-11 14:07:27 +0200

    Added the ability to move the enemy to first way point

--- Scripts/Characters/Enemy/EnemyKnight.cs
+++ Scripts/Characters/Enemy/EnemyKnight.cs
@@ -3,4 +3,16 @@ using System;
 
 public partial class EnemyKnight : Character
 {
+    [Export]
+    protected Path3D _pathNode;
+
+    
+    
+    
+    // Getters and Setters-------------------------------------------------------------------------
+
+    /// <summary>
+    /// Getter fo the path Node
+    /// </summary>
+    public Path3D PathNode { get => _pathNode; }
 }
--- Scripts/Characters/Enemy/EnemyReturnState.cs
+++ Scripts/Characters/Enemy/EnemyReturnState.cs
@@ -4,8 +4,19 @@ using System;
 
 public partial class EnemyReturnState : EnemyStateMachine
 {
+    private Vector3 _destination;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _destination = _enemyNode.PathNode.Curve.GetPointPosition(0);    
+    }
+
+
     protected override void EnterCurrentState()
     {
         _enemyNode.AnimationPlayer.Play(EnemyAnimationConstants.MOVE);
+        _enemyNode.Position = _destination;
     }
 }
