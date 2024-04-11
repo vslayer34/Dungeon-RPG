5098504c4d3ff8b38f7b17595de078488a96f75c
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-11 14:32:58 +0200

    Moved the enemy to the next waypoint instead of teleporting them there

--- Scripts/Characters/Enemy/EnemyReturnState.cs
+++ Scripts/Characters/Enemy/EnemyReturnState.cs
@@ -10,13 +10,44 @@ public partial class EnemyReturnState : EnemyStateMachine
     {
         base._Ready();
 
-        _destination = _enemyNode.PathNode.Curve.GetPointPosition(0);    
+        // _destination = new Vector3(
+        //     _enemyNode.PathNode.Curve.GetPointPosition(0).X, 
+        //     _enemyNode.Position.Y,
+        //     _enemyNode.PathNode.Curve.GetPointPosition(0).Z);
+
+        // _destination = _enemyNode.PathNode.Curve.GetPointPosition(0);
+        _destination = _enemyNode.PathNode.GlobalPosition + _enemyNode.PathNode.Curve.GetPointPosition(0);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        MoveToDestination();
     }
 
 
     protected override void EnterCurrentState()
     {
         _enemyNode.AnimationPlayer.Play(EnemyAnimationConstants.MOVE);
-        _enemyNode.Position = _destination;
+        // _enemyNode.Position = _destination;
+    }
+
+
+    /// <summary>
+    /// MOve the enemy to the waypoint position
+    /// </summary>
+    private void MoveToDestination()
+    {
+        if (_enemyNode.Position == _destination)
+        {
+            GD.Print("Reached final destination");
+            return;
+        }
+
+        GD.Print(_destination);
+        _enemyNode.Velocity = _enemyNode.Position.DirectionTo(_destination);
+
+        _enemyNode.MoveAndSlide();
     }
 }
