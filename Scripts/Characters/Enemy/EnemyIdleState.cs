adfcdc0144bf905b201a7afbdd512c65b1338878
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-14 18:00:31 +0200

    Switched the idle state to return state at the start of the game

--- Scripts/Characters/Enemy/EnemyIdleState.cs
+++ Scripts/Characters/Enemy/EnemyIdleState.cs
@@ -8,4 +8,10 @@ public partial class EnemyIdleState : EnemyStateMachine
     {
         _enemyNode.AnimationPlayer.Play(EnemyAnimationConstants.IDLE);
     }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        _enemyNode.StateMachine.SwitchState<EnemyReturnState>();
+    }
 }
