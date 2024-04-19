94174663d5681622b4d1c048877fd8c11f5c1911
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-19 21:51:12 +0200

    Changed the collision layers for the enemy and added the ability to detect the player and chase them during the chase state

--- Scripts/Characters/Enemy/EnemyChaseState.cs
+++ Scripts/Characters/Enemy/EnemyChaseState.cs
@@ -1,12 +1,25 @@
 using DungeonRPG.Scripts.Helper;
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyChaseState : EnemyStateMachine
 {
+    private CharacterBody3D _targetPlayer;
+
+
     protected override void EnterCurrentState()
     {
         _enemyNode.AnimationPlayer.Play(EnemyAnimationConstants.MOVE);
-        GD.Print("Chase");
+        _targetPlayer = _enemyNode.DetectionArea.GetOverlappingBodies().First() as CharacterBody3D;
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        _destination = _targetPlayer.GlobalPosition;
+        _enemyNode.NavAgent.TargetPosition = _destination;
+
+        MoveToDestination();
     }
 }
